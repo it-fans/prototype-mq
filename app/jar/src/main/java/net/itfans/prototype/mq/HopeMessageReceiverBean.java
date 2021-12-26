c17aed2b467c10e652fe81956a17dc7ee96033c4
LAPTOP-597TA2N2\lizheng <lizheng@outlook.jp>
2021-12-27 01:33:25 +0900

    try exception

--- app/jar/src/main/java/net/itfans/prototype/mq/HopeMessageReceiverBean.java
+++ app/jar/src/main/java/net/itfans/prototype/mq/HopeMessageReceiverBean.java
@@ -3,14 +3,27 @@ package net.itfans.prototype.mq;
 
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.TextMessage;
 
 @MessageDriven(activationConfig = {@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")})
 public class HopeMessageReceiverBean implements MessageListener {
     
     public void onMessage(Message message) {
-        System.out.println("Received Message:" + message.toString());
+        try {
+            String text = ((TextMessage)message).getText();
+            System.out.println("Received Message:" + text);
+
+            if ("xxx".equals(text)) {
+                throw new RuntimeException("data error");
+            }
+            Thread.sleep(1000);
+        } catch (JMSException | InterruptedException e) {
+            throw new RuntimeException("Unexpected Exception");
+        }
+
     }
 
 }
